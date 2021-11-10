18cb317b804964f187ad5c4935a02e77c7d2c408
Ali Uneri <ali.uneri@jhu.edu>
2021-11-10 11:11:57 -0500

    Allow passing slurm instance to magics

--- ipyslurm/magic.py
+++ ipyslurm/magic.py
@@ -17,12 +17,14 @@ class SlurmMagics(magic.Magics):
     @magic_arguments.magic_arguments()
     @magic_arguments.argument('--args', nargs='*', help='Additional arguments to sbatch')
     @magic_arguments.argument('--job', help='Store job ID in variable', metavar='VARIABLE')
-    @magic.cell_magic
+    @magic_arguments.argument('--instance', help='Existing slurm instance', metavar='VARIABLE')
     @magic.needs_local_scope
+    @magic.cell_magic
     def sbatch(self, line, cell, local_ns):
         """Submit a batch script to Slurm."""
         args = magic_arguments.parse_argstring(self.sbatch, line)
-        job = self._slurm.sbatch(cell, args.args)
+        slurm = local_ns.get(args.instance, self._slurm)
+        job = slurm.sbatch(cell, args.args)
         print(f'Submitted batch job {job}')
         if args.job is not None:
             local_ns.update({args.job: job})
@@ -31,15 +33,17 @@ class SlurmMagics(magic.Magics):
     @magic_arguments.argument('--period', type=float, help='Repeat execution with a given periodicity', metavar='SECONDS')
     @magic_arguments.argument('--timeout', type=float, help='Timeout for when used with --period', metavar='SECONDS')
     @magic_arguments.argument('--stdout', help='Store stdout in variable', metavar='VARIABLE')
-    @magic.cell_magic
+    @magic_arguments.argument('--instance', help='Existing slurm instance', metavar='VARIABLE')
     @magic.needs_local_scope
+    @magic.cell_magic
     def scommand(self, line, cell, local_ns):
         """Execute commands on server."""
         args = magic_arguments.parse_argstring(self.scommand, line)
+        slurm = local_ns.get(args.instance, self._slurm)
         start = timeit.default_timer()
         try:
             while True:
-                stdout = self._slurm.command(cell)
+                stdout = slurm.command(cell)
                 clear_output(wait=True)
                 print(stdout)
                 elapsed = timeit.default_timer() - start
@@ -55,37 +59,38 @@ class SlurmMagics(magic.Magics):
             local_ns.update({args.stdout: stdout})
 
     @magic_arguments.magic_arguments()
+    @magic_arguments.argument('--instance', help='Existing slurm instance', metavar='VARIABLE')
+    @magic.needs_local_scope
     @magic.cell_magic
-    def sftp(self, line, cell):
+    def sftp(self, line, cell, local_ns):
         """File transfer over secure shell.
 
         Supported commands: cd, chmod, chown, get, lcd, lls, lmkdir, ln, lpwd, lrm, lrmdir, ls, mkdir, put, pwd, rename, rm, rmdir, symlink.
         See http://man.openbsd.org/sftp#INTERACTIVE_COMMANDS for details.
         """
-        magic_arguments.parse_argstring(self.sftp, line)
-        self._slurm.sftp(cell)
+        args = magic_arguments.parse_argstring(self.sftp, line)
+        slurm = local_ns.get(args.instance, self._slurm)
+        slurm.sftp(cell)
 
     @magic_arguments.magic_arguments()
+    @magic_arguments.argument('--instance', help='Existing slurm instance', metavar='VARIABLE')
+    @magic.needs_local_scope
     @magic.line_magic
-    def sinteract(self, line):
+    def sinteract(self, line, local_ns):
         """Interactive shell."""
-        magic_arguments.parse_argstring(self.sinteract, line)
-        self._slurm.interact()
+        args = magic_arguments.parse_argstring(self.sinteract, line)
+        slurm = local_ns.get(args.instance, self._slurm)
+        slurm.interact()
 
     @magic_arguments.magic_arguments()
     @magic_arguments.argument('--server', help='Address of server', metavar='ADDRESS')
     @magic_arguments.argument('--username', help='Username, interactively requested if not provided')
     @magic_arguments.argument('--password', help='Password, interactively requested if not provided')
-    @magic_arguments.argument('--instance', help='An existing slurm instance', metavar='VARIABLE')
     @magic.line_magic
-    @magic.needs_local_scope
-    def slogin(self, line, local_ns):
+    def slogin(self, line):
         """Login to server."""
         args = magic_arguments.parse_argstring(self.slogin, line)
-        if args.instance is not None:
-            self._slurm = local_ns.get(args.instance)
-        else:
-            self._slurm.login(args.server, args.username, args.password)
+        self._slurm.login(args.server, args.username, args.password)
 
     @magic_arguments.magic_arguments()
     @magic.line_magic
@@ -97,16 +102,22 @@ class SlurmMagics(magic.Magics):
     @magic_arguments.magic_arguments()
     @magic_arguments.argument('job', help='Job ID')
     @magic_arguments.argument('--lines', default=1, type=int, help='Print last N lines of the log', metavar='N')
+    @magic_arguments.argument('--instance', help='Existing slurm instance', metavar='VARIABLE')
+    @magic.needs_local_scope
     @magic.line_magic
-    def stail(self, line):
+    def stail(self, line, local_ns):
         """Logout of server."""
         args = magic_arguments.parse_argstring(self.stail, line)
-        self._slurm.tail(args.job, lines=args.lines)
+        slurm = local_ns.get(args.instance, self._slurm)
+        slurm.tail(args.job, lines=args.lines)
 
     @magic_arguments.magic_arguments()
     @magic_arguments.argument('filepath', help='Path of file')
     @magic_arguments.argument('--append', action='store_true', help='Append contents of the cell to an existing file')
+    @magic_arguments.argument('--instance', help='Existing slurm instance', metavar='VARIABLE')
+    @magic.needs_local_scope
     @magic.cell_magic
-    def swritefile(self, line, cell):
+    def swritefile(self, line, cell, local_ns):
         args = magic_arguments.parse_argstring(self.swritefile, line)
-        self._slurm.writefile(args.filepath, cell, append=args.append)
+        slurm = local_ns.get(args.instance, self._slurm)
+        slurm.writefile(args.filepath, cell, append=args.append)
