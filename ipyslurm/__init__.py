524d1fedd384afbeceb94b600335a758387fde1e
Ali Uneri <auneri@users.noreply.github.com>
2024-11-30 15:31:38 -0500

    Mark ipywidgets as an optional dependency

--- ipyslurm/__init__.py
+++ ipyslurm/__init__.py
@@ -1,7 +1,10 @@
 from .version import __version__  # noqa: F401
-from .magic import SlurmMagics  # noqa: I100
-from .slurm import Slurm  # noqa: F401
+from .slurm import Slurm  # noqa: F401, I100
 
+try:
+    from .magic import SlurmMagics
 
-def load_ipython_extension(ipython):
-    ipython.register_magics(SlurmMagics)
+    def load_ipython_extension(ipython):
+        ipython.register_magics(SlurmMagics)
+except ModuleNotFoundError:
+    pass
